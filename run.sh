81d58a4a9f0792f306d0f4a98a8b9a287001a56b
vitelot <v.servedio@gmail.com>
2025-12-03 19:39:38 +0100

    script now conforms to the new -i -o -p params

--- run.sh
+++ run.sh
@@ -4,15 +4,32 @@ echo "Enter the number of threads you wish to use:"
 read nthreads
 echo 
 
-if [ $# = 0 ]; then
-    echo "No filenames provided. Continuing with default data/test_list.csv and data/output.csv"
-elif [ $# = 1 ]; then
-    echo "No output filename provided. Continuing with default data/output.csv"
-elif [ $# = 2 ]; then
+# Define defaults for clarity
+default_in="data/test_list.csv"
+default_out="data/output.csv"
+
+if [ $# -eq 0 ]; then
+    echo "No filenames provided. Continuing with default $default_in and $default_out"
+    in="$default_in"
+    out="$default_out"
+    psi=""
+elif [ $# -eq 1 ]; then
+    echo "No output filename provided. Continuing with default $default_out"
+    in="$1"
+    out="$default_out"
+    psi=""
+elif [ $# -eq 2 ]; then
     echo "Running ESRI in Julia with input file ${1} and output ${2}"
-elif [ $# = 3 ]; then
-    echo "Running ESRI in Julia with input file ${1} and output ${2} and psi mat defined in ${3}"
+    in="$1"
+    out="$2"
+    psi=""
+elif [ $# -eq 3 ]; then
+    echo "Running ESRI in Julia with input file ${1}, output ${2}, and psi mat ${3}"
+    in="$1"
+    out="$2"
+    psi="$3"
 fi
 
-echo "julia --project --threads $nthreads esri.jl $@"
-julia --project --threads $nthreads esri.jl $@
+
+echo "julia --project --threads $nthreads esri.jl -i $in -o $out -p \"$psi\""
+julia --project --threads $nthreads esri.jl -i "$in" -o "$out" -p "$psi"
